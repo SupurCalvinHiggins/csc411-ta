8acc833e5c8edc82f3bbc22f5e4930f7df9a7f81
Calvin Higgins <calvin_higgins2@uri.edu>
2025-03-25 14:01:05 -0400

    Fixed project structure.

--- hw6/main.c
+++ hw6/main.c
@@ -2,6 +2,9 @@
 #include "rv_stdio.h"
 #include "rv_str.h"
 
+int g_uninit;
+int g_init = 1;
+
 int main(int argc, char *argv[]) {
   rv_putc('h');
   rv_puts("ello world!\n");
@@ -27,6 +30,13 @@ int main(int argc, char *argv[]) {
   buf[1] = '0';
   buf[2] = 0;
   rv_assert(rv_str_to_int(buf) == 0);
+
+  rv_assert(argc == 1);
+  rv_puts(argv[0]);
+  rv_putc('\n');
+
+  rv_assert(g_uninit == 0);
+  rv_assert(g_init == 1);
 
   while (1)
     ;
